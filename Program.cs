662c4dbdef52a3038f7f020b21d9a80fe3c467b6
kubuzbusra <kubuzbusra@gmail.com>
2016-11-22 14:06:49 +0300

    added project

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace PizzaSiparis
         /// </summary>
         [STAThread]
         static void Main()
+
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
